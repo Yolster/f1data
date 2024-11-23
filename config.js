1cdfdbd7063c9272b79fe1594c70e6113f7e80d6
Yolster <80517257+Yolster@users.noreply.github.com>
2024-11-23 13:51:55 +0300

    Update config.js
    Delete personal things and add a comment for users

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "prefix": "f1.",
-    "owner": "800809750584492052",
-    "token": "ODAxNDU4Mzk0NTczNTcwMDU4.GTnT0w.yTw-kltFD8jLDk-xrp8PrxZUnUtjs8axHh7IZk"
+    "prefix": "Your bot prefix",
+    "owner": "Your Discord id",
+    "token": "Your Discord bot token"
   }
